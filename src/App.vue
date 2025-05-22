815af42db05a786b723610ff2ab54bece7d3cfa8
VladislavG <grishvm@yandex.ru>
2025-05-22 20:30:06 +0500

    fade-анимация перехода от карточки в списке к детальной странице

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,22 @@ import {RouterView} from 'vue-router';
 
 <template>
 		<div class="container">
-				<RouterView/>
+				<Transition name="fade" mode="out-in">
+						<router-view />
+				</Transition>
 		</div>
 </template>
 
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+    transition: opacity 1.5s ease;
+}
+
+.fade-enter-from, .fade-leave-to {
+    opacity: 0;
+}
+
+.fade-enter-to, .fade-leave-from {
+    opacity: 1;
+}
+</style>
